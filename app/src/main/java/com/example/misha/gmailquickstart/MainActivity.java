47b9efb548f8e649aa3bf1834a9358ae72de5f76
Mykhaylo Kharaba <mykhaylo.kharaba@eleks.com>
2015-10-23 19:20:18 +0300

    +

--- app/src/main/java/com/example/misha/gmailquickstart/MainActivity.java
+++ app/src/main/java/com/example/misha/gmailquickstart/MainActivity.java
@@ -38,7 +38,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
     GoogleAccountCredential mCredential;
     private TextView mOutputText;
     ProgressDialog mProgress;
@@ -47,14 +48,16 @@ public class MainActivity extends Activity {
     static final int REQUEST_AUTHORIZATION = 1001;
     static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
     private static final String PREF_ACCOUNT_NAME = "accountName";
-    private static final String[] SCOPES = { GmailScopes.GMAIL_LABELS };
+    private static final String[] SCOPES = {GmailScopes.GMAIL_LABELS};
 
     /**
      * Create the main activity.
+     *
      * @param savedInstanceState previously saved instance data.
      */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         LinearLayout activityLayout = new LinearLayout(this);
         LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
@@ -94,13 +97,16 @@ public class MainActivity extends Activity {
      * a call to onCreate().
      */
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
-        if (isGooglePlayServicesAvailable()) {
+        if (isGooglePlayServicesAvailable())
+        {
             refreshResults();
-        } else {
-            mOutputText.setText("Google Play Services required: " +
-                    "after installing, close and relaunch this app.");
+        }
+        else
+        {
+            mOutputText.setText("Google Play Services required: after installing, close and relaunch this app.");
         }
     }
 
@@ -108,28 +114,32 @@ public class MainActivity extends Activity {
      * Called when an activity launched here (specifically, AccountPicker
      * and authorization) exits, giving you the requestCode you started it with,
      * the resultCode it returned, and any additional data from it.
+     *
      * @param requestCode code indicating which activity result is incoming.
-     * @param resultCode code indicating the result of the incoming
-     *     activity result.
-     * @param data Intent (containing result data) returned by incoming
-     *     activity result.
+     * @param resultCode  code indicating the result of the incoming
+     *                    activity result.
+     * @param data        Intent (containing result data) returned by incoming
+     *                    activity result.
      */
     @Override
     protected void onActivityResult(
-            int requestCode, int resultCode, Intent data) {
+            int requestCode, int resultCode, Intent data)
+    {
         super.onActivityResult(requestCode, resultCode, data);
-        switch(requestCode) {
+        switch (requestCode)
+        {
             case REQUEST_GOOGLE_PLAY_SERVICES:
-                if (resultCode != RESULT_OK) {
+                if (resultCode != RESULT_OK)
+                {
                     isGooglePlayServicesAvailable();
                 }
                 break;
             case REQUEST_ACCOUNT_PICKER:
-                if (resultCode == RESULT_OK && data != null &&
-                        data.getExtras() != null) {
-                    String accountName =
-                            data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
-                    if (accountName != null) {
+                if (resultCode == RESULT_OK && data != null && data.getExtras() != null)
+                {
+                    String accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+                    if (accountName != null)
+                    {
                         mCredential.setSelectedAccountName(accountName);
                         SharedPreferences settings =
                                 getPreferences(Context.MODE_PRIVATE);
@@ -137,12 +147,15 @@ public class MainActivity extends Activity {
                         editor.putString(PREF_ACCOUNT_NAME, accountName);
                         editor.apply();
                     }
-                } else if (resultCode == RESULT_CANCELED) {
+                }
+                else if (resultCode == RESULT_CANCELED)
+                {
                     mOutputText.setText("Account unspecified.");
                 }
                 break;
             case REQUEST_AUTHORIZATION:
-                if (resultCode != RESULT_OK) {
+                if (resultCode != RESULT_OK)
+                {
                     chooseAccount();
                 }
                 break;
@@ -156,13 +169,20 @@ public class MainActivity extends Activity {
      * email address isn't known yet, then call chooseAccount() method so the
      * user can pick an account.
      */
-    private void refreshResults() {
-        if (mCredential.getSelectedAccountName() == null) {
+    private void refreshResults()
+    {
+        if (mCredential.getSelectedAccountName() == null)
+        {
             chooseAccount();
-        } else {
-            if (isDeviceOnline()) {
+        }
+        else
+        {
+            if (isDeviceOnline())
+            {
                 new MakeRequestTask(mCredential).execute();
-            } else {
+            }
+            else
+            {
                 mOutputText.setText("No network connection available.");
             }
         }
@@ -172,16 +192,18 @@ public class MainActivity extends Activity {
      * Starts an activity in Google Play Services so the user can pick an
      * account.
      */
-    private void chooseAccount() {
-        startActivityForResult(
-                mCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+    private void chooseAccount()
+    {
+        startActivityForResult(mCredential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
     }
 
     /**
      * Checks whether the device currently has a network connection.
+     *
      * @return true if the device has a network connection, false otherwise.
      */
-    private boolean isDeviceOnline() {
+    private boolean isDeviceOnline()
+    {
         ConnectivityManager connMgr =
                 (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
@@ -192,16 +214,21 @@ public class MainActivity extends Activity {
      * Check that Google Play services APK is installed and up to date. Will
      * launch an error dialog for the user to update Google Play Services if
      * possible.
+     *
      * @return true if Google Play Services is available and up to
-     *     date on this device; false otherwise.
+     * date on this device; false otherwise.
      */
-    private boolean isGooglePlayServicesAvailable() {
+    private boolean isGooglePlayServicesAvailable()
+    {
         final int connectionStatusCode =
                 GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode))
+        {
             showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
             return false;
-        } else if (connectionStatusCode != ConnectionResult.SUCCESS ) {
+        }
+        else if (connectionStatusCode != ConnectionResult.SUCCESS)
+        {
             return false;
         }
         return true;
@@ -210,11 +237,13 @@ public class MainActivity extends Activity {
     /**
      * Display an error dialog showing that Google Play Services is missing
      * or out of date.
+     *
      * @param connectionStatusCode code describing the presence (or lack of)
-     *     Google Play Services on this device.
+     *                             Google Play Services on this device.
      */
     void showGooglePlayServicesAvailabilityErrorDialog(
-            final int connectionStatusCode) {
+            final int connectionStatusCode)
+    {
         Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
                 connectionStatusCode,
                 MainActivity.this,
@@ -226,11 +255,13 @@ public class MainActivity extends Activity {
      * An asynchronous task that handles the Gmail API call.
      * Placing the API calls in their own task ensures the UI stays responsive.
      */
-    private class MakeRequestTask extends AsyncTask<Void, Void, List<String>> {
+    private class MakeRequestTask extends AsyncTask<Void, Void, List<String>>
+    {
         private com.google.api.services.gmail.Gmail mService = null;
         private Exception mLastError = null;
 
-        public MakeRequestTask(GoogleAccountCredential credential) {
+        public MakeRequestTask(GoogleAccountCredential credential)
+        {
             HttpTransport transport = AndroidHttp.newCompatibleTransport();
             JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
             mService = new com.google.api.services.gmail.Gmail.Builder(
@@ -241,13 +272,18 @@ public class MainActivity extends Activity {
 
         /**
          * Background task to call Gmail API.
+         *
          * @param params no parameters needed for this task.
          */
         @Override
-        protected List<String> doInBackground(Void... params) {
-            try {
+        protected List<String> doInBackground(Void... params)
+        {
+            try
+            {
                 return getDataFromApi();
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
                 mLastError = e;
                 cancel(true);
                 return null;
@@ -256,16 +292,19 @@ public class MainActivity extends Activity {
 
         /**
          * Fetch a list of Gmail labels attached to the specified account.
+         *
          * @return List of Strings labels.
          * @throws IOException
          */
-        private List<String> getDataFromApi() throws IOException {
+        private List<String> getDataFromApi() throws IOException
+        {
             // Get the labels in the user's account.
             String user = "me";
             List<String> labels = new ArrayList<String>();
             ListLabelsResponse listResponse =
                     mService.users().labels().list(user).execute();
-            for (Label label : listResponse.getLabels()) {
+            for (Label label : listResponse.getLabels())
+            {
                 labels.add(label.getName());
             }
             return labels;
@@ -273,39 +312,53 @@ public class MainActivity extends Activity {
 
 
         @Override
-        protected void onPreExecute() {
+        protected void onPreExecute()
+        {
             mOutputText.setText("");
             mProgress.show();
         }
 
         @Override
-        protected void onPostExecute(List<String> output) {
+        protected void onPostExecute(List<String> output)
+        {
             mProgress.hide();
-            if (output == null || output.size() == 0) {
+            if (output == null || output.size() == 0)
+            {
                 mOutputText.setText("No results returned.");
-            } else {
+            }
+            else
+            {
                 output.add(0, "Data retrieved using the Gmail API:");
                 mOutputText.setText(TextUtils.join("\n", output));
             }
         }
 
         @Override
-        protected void onCancelled() {
+        protected void onCancelled()
+        {
             mProgress.hide();
-            if (mLastError != null) {
-                if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+            if (mLastError != null)
+            {
+                if (mLastError instanceof GooglePlayServicesAvailabilityIOException)
+                {
                     showGooglePlayServicesAvailabilityErrorDialog(
                             ((GooglePlayServicesAvailabilityIOException) mLastError)
                                     .getConnectionStatusCode());
-                } else if (mLastError instanceof UserRecoverableAuthIOException) {
+                }
+                else if (mLastError instanceof UserRecoverableAuthIOException)
+                {
                     startActivityForResult(
                             ((UserRecoverableAuthIOException) mLastError).getIntent(),
                             MainActivity.REQUEST_AUTHORIZATION);
-                } else {
+                }
+                else
+                {
                     mOutputText.setText("The following error occurred:\n"
                             + mLastError.getMessage());
                 }
-            } else {
+            }
+            else
+            {
                 mOutputText.setText("Request cancelled.");
             }
         }
